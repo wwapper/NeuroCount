213f19cef04c3f2fd6d023117ef9ecb14b39b6cf
pk <chemkpv@gmail.com>
2022-12-28 20:29:10 +0700

    Initial commit

--- program/counter.py
+++ program/counter.py
@@ -21,7 +21,7 @@ image = cv2.filter2D(image, -1, kernel)
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imwrite("gray.jpg", gray_image)
 gray_image = cv2.GaussianBlur(gray_image,(3,3),5)
-ret, threshold_image = cv2.threshold(gray_image, 110, 255, 0)
+ret, threshold_image = cv2.threshold(gray_image, 115, 255, 0)
 cv2.imwrite("threshold_image.jpg", threshold_image)
 
 #контурирование
@@ -37,24 +37,35 @@ nb_blobs, im_with_separated_blobs, stats, _ = cv2.connectedComponentsWithStats(e
 sizes = stats[:, -1]
 sizes = sizes[1:]
 nb_blobs -= 1
-min_size = 90
+min_size = 80
 im_result = np.zeros((edged.shape))
 for blob in range(nb_blobs):
-    if sizes[blob] >= min_size:
+    if 500 >= sizes[blob] >= min_size:
         im_result[im_with_separated_blobs == blob + 1] = 255
 cv2.imwrite("final.jpg", im_result)
 
-#контурирование
+image1 = image.copy()
 im_result = im_result.astype(np.uint8)
 contours, hierarchy = cv2.findContours(im_result.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-# отображаем контуры поверх изображения
-cv2.drawContours(image, contours, -1, (0,0,0), 3, cv2.LINE_AA, hierarchy, 1)
-cv2.imshow('contours', image) # выводим итоговое изображение в окно
-cv2.waitKey()
+cv2.drawContours(image1, contours, -1, (0,0,0), 3, cv2.LINE_AA)
+cv2.imwrite('contours_old.jpg', image1) # выводим итоговое изображение в окно
 
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7))
 closed_final = cv2.morphologyEx(im_result, cv2.MORPH_CLOSE, kernel)
 cv2.imwrite("closed_final.jpg", closed_final)
+
+#контурирование
+closed_final = closed_final.astype(np.uint8)
+contours, hierarchy = cv2.findContours(closed_final.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+new_contours = []
+for cnt in contours:
+    area = cv2.contourArea(cnt)
+    perimeter = cv2.arcLength(cnt, True)
+    if area - perimeter > 100:
+        new_contours.append(cnt)
+# отображаем контуры поверх изображения
+cv2.drawContours(image, new_contours, -1, (0,0,0), 3, cv2.LINE_AA)
+cv2.imwrite('contours_new.jpg', image) # выводим итоговое изображение в окно
 
 closed_final = closed_final.astype(np.uint8)
 cnts = cv2.findContours(closed_final.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
